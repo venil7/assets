e47507ee48ad89f8c576fd971c6ae84bb3568c13
darkruby <art@darkruby.co.uk>
2026-02-04 22:57:51 +0000

    minot sql fix

--- .migrations/005_num_shares.up.sql
+++ .migrations/005_num_shares.up.sql
@@ -22,33 +22,22 @@ with
     select
       t.*,
       case
+        when tc.total_quantity = 0 then 0
         when t.type = 'buy' then (t.quantity / tc.total_quantity)
-        else - (t.quantity / tc.total_quantity)
+        else (- t.quantity / tc.total_quantity)
       end AS contribution,
       SUM(
         CASE
           WHEN t.type = 'buy' THEN t.quantity
           ELSE - t.quantity
         END
-      ) OVER (
-        partition by
-          t.asset_id,
-          p.user_id
-        ORDER BY
-          date
-      ) AS holdings,
+      ) OVER user_asset_txs AS holdings, --holdings post-transaction
       SUM(
         CASE
           WHEN t.type = 'buy' THEN t.quantity * t.price
           ELSE - t.quantity * t.price
         END
-      ) OVER (
-        partition by
-          t.asset_id,
-          p.user_id
-        ORDER BY
-          date
-      ) AS total_invested,
+      ) OVER user_asset_txs AS total_invested, -- invested post-transaction
       a.name,
       a.ticker,
       p.name as portfolio_name,
@@ -59,12 +48,21 @@ with
       inner join total_contr tc on tc.asset_id = t.asset_id
       inner join assets a on a.id = t.asset_id
       inner join portfolios p on p.id = a.portfolio_id
+    window
+      user_asset_txs as (
+        partition by
+          t.asset_id,
+          p.user_id
+        ORDER BY
+          date rows between unbounded preceding
+          and current row
+      )
   )
 select
   *,
   case
     when holdings = 0 then null
     else total_invested / holdings
-  end as avg_price
+  end as avg_price --avg unit cost post-transaction
 from
   subquery;
