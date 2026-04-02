3d5f3ebc99c06945a956b0be8fab8ca21b207577
darkruby <art@darkruby.co.uk>
2026-04-02 20:02:28 +0100

    prefs update modified

--- packages/backend/src/repository/sql/prefs/update.sql
+++ packages/backend/src/repository/sql/prefs/update.sql
@@ -1,3 +1,6 @@
 update prefs
-set base_ccy = $base_ccy, additional = $additional
+set
+  base_ccy = $base_ccy,
+  additional = $additional,
+  modified = datetime('subsec')
 where user_id = $userId;
